8e0e45b3af3f0688a5fc2aa02504d2ed7f48ebd5
“[Зорин <[evgenijzorin@bk.ru]>
2022-09-01 13:24:41 +0300

    homework for lesson_3 completed

---
+++ Lesson_3/CardStorageService/CardStorageService.Data/AccountSession.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardStorageService.Data
+{
+    [Table("AccountSessions")]
+    public class AccountSession
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)] // генерируется базой данных
+        public int SessionId { get; set; }
+        [Required] // обязательный требуемый
+        [StringLength(384)]
+        public string SessionToken { get; set; } // сессионный ключ (токен пользователя)
+
+        [ForeignKey(nameof(Account))] // ссылается на аккаунт
+        public int AccountId { get; set; } // идентификатор пользователя
+        [Column(TypeName = "datetime2")]
+        public DateTime TimeCreated { get; set; } // время создания
+        [Column(TypeName = "datetime2")]
+        public DateTime TimeLastRequest { get; set; } // время последнего запроса
+        public bool IsClosed { get; set; } // завершена ли сессия (logout)
+        [Column(TypeName = "datetime2")]
+        public DateTime? TimeClosed{ get; set; } // время завершения сессии
+        
+
+
+
+        public virtual Account Account { get; set; }
+
+    }
+}
---
+++ Lesson_3/CardStorageService/CardStorageService/Models/Requests/CreateCardRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CardStorageService.Models.Requests
+{
+    // Создать запрос для добавления новой карты 
+    public class CreateCardRequest
+    {
+        public int ClientId { get; set; }
+        public string CardNo { get; set; }
+        public string? Name { get; set; }
+        public string? CVV2 { get; set; }
+        public DateTime ExpirationDate { get; set; } // истечение срока
+    }
+}
