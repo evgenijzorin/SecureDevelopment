56b3e1c9f67afb5f1d2c936f60a5fb7d144549a3
“[Зорин <[evgenijzorin@bk.ru]>
2022-09-03 14:02:15 +0300

    home work for lesson 4 Completed

---
+++ Lesson_4N/CacheProvider/CacheProvider/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheProviderTest
+{
+    public class Car
+    {
+        public string NameModel { get; set; }
+        public string Owner { get; set; }
+        public int Id { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
